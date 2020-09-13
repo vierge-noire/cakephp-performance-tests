c1fe70aa024c8a000a9af6ebb6e9f5f407e3a8bd
Juan Pablo Ramirez <juan-pablo.ramirez@postcon.de>
2020-09-13 23:15:43 +0200

    Test fixed

--- tests/TestCase/StaticFixturesTest.php
+++ tests/TestCase/StaticFixturesTest.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 namespace App\Test\TestCase;
 
-use App\Test\Fixture\Table0Fixture;
 use App\Test\FixturesMaker;
 use Cake\ORM\Entity;
 use Cake\ORM\TableRegistry;
@@ -17,7 +16,7 @@ class StaticFixturesTest extends TestCase
     public function getFixtures(): array
     {
         for ($i=0; $i<FixturesMaker::NUMBER_OF_TABLES_PER_TEST; $i++) {
-            $this->addFixture("app.table$i"."s");
+            $this->addFixture("app.Table$i"."s");
         }
 
         return parent::getFixtures();
