14655a362abfc05ae14871fcdfbb853c0f7f9fc1
Juan Pablo Ramirez <juan-pablo.ramirez@postcon.de>
2020-09-16 15:42:59 +0200

    Tests fixed

--- config/app_local.php
+++ config/app_local.php
@@ -36,7 +36,7 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'host' => 'mysql',
+            'host' => getenv('DB_HOST'),
             /*
              * CakePHP will use the default DB port based on the driver selected
              * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
@@ -63,7 +63,7 @@ return [
          * The test connection is used during the test suite.
          */
         'test' => [
-            'host' => 'mysql',
+            'host' => getenv('DB_HOST'),
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => 'root',
