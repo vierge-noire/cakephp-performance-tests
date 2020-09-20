3779acd9a9c561acf14bf0a560b29e4bc8d87cd0
Juan Pablo Ramirez <juan-pablo.ramirez@postcon.de>
2020-09-20 15:34:10 +0200

    Tests updated

--- tests/TestCase/DynamicFixturesPersistedTest.php
+++ tests/TestCase/DynamicFixturesPersistedTest.php
@@ -4,15 +4,6 @@ declare(strict_types=1);
 namespace App\Test\TestCase;
 
 use App\Test\Factory\Table0Factory;
-use App\Test\Factory\Table1Factory;
-use App\Test\Factory\Table2Factory;
-use App\Test\Factory\Table3Factory;
-use App\Test\Factory\Table4Factory;
-use App\Test\Factory\Table5Factory;
-use App\Test\Factory\Table6Factory;
-use App\Test\Factory\Table7Factory;
-use App\Test\Factory\Table8Factory;
-use App\Test\Factory\Table9Factory;
 use App\Test\FixturesMaker;
 use Cake\ORM\TableRegistry;
 use Cake\TestSuite\TestCase;
